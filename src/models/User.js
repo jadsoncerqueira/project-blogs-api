7597f38939d46b30d6add65aced8d322476e4f71
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-10-27 23:18:45 -0300

    requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+const userModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    underscored: true,
+  });
+  
+  return User;
+};
+  
+module.exports = userModel;
